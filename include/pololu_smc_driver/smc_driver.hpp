97986bbe50d799a6bd43ecd25ce0741c37fddec8
Scott K Logan <logans@cottsay.net>
2013-01-17 22:29:36 -0700

    Initial commit

---
+++ include/pololu_smc_driver/smc_driver.hpp
@@ -0,0 +1,318 @@
+/***************************************************************************//**
+ * \file smc_driver.hpp
+ *
+ * \brief ROS Implementation of the C Driver (header)
+ * \author Scott K Logan
+ * \date January 07, 2013
+ *
+ * API for the ROS driver
+ *
+ * \section license License (BSD-3)
+ * Copyright (c) 2013, Scott K Logan\n
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of Willow Garage, Inc. nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+#ifndef _smc_driver_hpp
+#define _smc_driver_hpp
+
+#include "pololu_smc_driver/smc.h"
+#include "pololu_smc_driver/SMCDriverConfig.h"
+
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
+#include <std_srvs/Empty.h>
+#include <dynamic_reconfigure/server.h>
+#include <diagnostic_updater/diagnostic_updater.h>
+#include <diagnostic_updater/publisher.h>
+#include <diagnostic_updater/update_functions.h>
+
+/*!
+ * \brief Used for the ROS SMC Driver.
+ *
+ * All elements of the ROS implementation including the wrapped driver and the
+ * dynamic reconfigure elements fall within this namespace.
+ */
+namespace pololu_smc_driver
+{
+	/*!
+	 * \brief ROS-wrapped implementation of the SMC driver.
+	 *
+	 * \author Scott K Logan
+	 *
+	 * This class interfaces with a single SMC device. It automatically subscribes
+	 * to the control feed and creates instances of the dynamic reconfigure server
+	 * and the diagnostic updater.
+	 */
+	class SMCDriver
+	{
+	public:
+		/*!
+		 * \brief Constructor.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * This constructor initializes the standalone C driver and the various
+		 * components of the ROS interface from that driver.
+		 *
+		 * \param _nh_priv Private node handle to use for all ROS communication
+		 *   within for this device. There should never be two drivers sharing the
+		 *   same private node handle. If one is not provided, it will be
+		 *   automatically created.
+		 * \param _serial Serial number of the target device. If no serial is given
+		 *   or an empty string is given, the first available device will be used.
+		 *   Please note that if there are multiple devices connected, this may not
+		 *   be the same device every time!
+		 */
+		SMCDriver( const ros::NodeHandle &_nh_priv = ros::NodeHandle( "~" ),
+			const std::string _serial = "" );
+		/*!
+		 * \brief Destructor.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * Closes the interface to the driver, unsubscribes, unadvertises, and frees
+		 * the standalone driver private data.
+		 */
+		~SMCDriver( );
+
+		/*!
+		 * \brief Opens the interface with the device.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * Calls the standalone driver routines for opening the USB communication
+		 * pipeline with the device. Each time this is called, the settings displayed
+		 * by dynamic reconfigure are updated according to the device settings. ROS
+		 * subscriptions and advertisements are made at this time.
+		 *
+		 * \returns True if device was successfully opened.
+		 */
+		bool SMCOpen( );
+		/*!
+		 * \brief Closes the interface with the device.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * Closes the non-diagnostic interfaces with ROS and closes the interface
+		 * with the driver and device.
+		 */
+		void SMCClose( );
+		/*!
+		 * \brief Communication status of the device.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * Tests for proper communication with the device driver.
+		 *
+		 * \returns False if there is a communications problem.
+		 */
+		bool SMCStat( );
+		/*!
+		 * \brief Sets the speed of the motor.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * Converts the generic float-style message to the driver-friendly values.
+		 * If the device is currently in a disconnected state, this function will
+		 * first attempt to connect to the device.
+		 *
+		 * \param spd Target speed for the device, between -1 and 1.
+		 *
+		 * \returns True on successful speed change.
+		 */
+		bool set_speed( float spd );
+	private:
+		/*!
+		 * \brief ROS message callback for setting the speed of the motor.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * Calls the native SMCDriver::set_speed function
+		 *
+		 * \param msg Float value indicating the desired speed. Should be between -1
+		 *   and 1.
+		 */
+		void SpeedCB( const std_msgs::Float32Ptr &msg );
+		/*!
+		 * \brief Dynamic Reconfigure Change Callback.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * When dynamic reconfigure values are changed this callback is called to
+		 * interface the new values with the device.
+		 *
+		 * \param cfg New dynamic reconfigure values to be sent to the device
+		 * \param lvl Level of change in the values (indicates if reset is necessary)
+		 */
+		void DynReCB( pololu_smc_driver::SMCDriverConfig &cfg, const uint32_t lvl );
+		/*!
+		 * \brief Callback for the Diagnostic Updater Timer
+		 *
+		 * \author Scott K Logan
+		 *
+		 * This timer is used to call the diagnostic update function at the configured
+		 * interval. This function simply calls that function.
+		 *
+		 * \param e Timer event (not used)
+		 */
+		void TimerCB( const ros::WallTimerEvent &e );
+		/*!
+		 * \brief Diagnostic update callback
+		 *
+		 * \author Scott K Logan
+		 *
+		 * Whenever the diagnostic_updater deems it necessary to update the values
+		 * therein, this callback is called to fetch the values from the device.
+		 *
+		 * \param[out] stat Structure in which to store the values for
+		 * diagnostic_updater to report
+		 */
+		void DiagCB( diagnostic_updater::DiagnosticStatusWrapper &stat );
+		/*!
+		 * \brief Service callback for bringing the motor out of safe start mode.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * This service callback causes the device to resume normal operation if
+		 * there has been a safe start violation.
+		 *
+		 * \param req Service request (not used)
+		 * \param[out] res Service response (not used)
+		 *
+		 * \returns True if request was successful
+		 */
+		bool SafeStartCB( std_srvs::Empty::Request &req, std_srvs::Empty::Response &res );
+		/*!
+		 * \brief Service callback for causing an Emergency Stop.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * This will call the driver to trigger the USB killswitch and cause a safe
+		 * start violation.
+		 *
+		 * \param req Service request (not used)
+		 * \param[out] res Service response (not used)
+		 *
+		 * \returns True if request was successful
+		 */
+		bool EStopCB( std_srvs::Empty::Request &req, std_srvs::Empty::Response &res );
+		/*!
+		 * \brief Update the values in Dynamic Reconfigure from the device.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * Queries the device for settings and updates the dynamic reconfigure
+		 * values appropriately. This is called automatically every time the device
+		 * is connected.
+		 *
+		 * \returns True if values were successfully updated.
+		 */
+		bool refresh_settings( );
+		/*!
+		 * \brief Converts an error code to a comma separated string.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * \param error Error value to convert
+		 *
+		 * \returns C++ string containing the error descriptions
+		 */
+		std::string ErrorToStr( const uint16_t error ) const;
+		/*!
+		 * \brief Converts a speed limit code to a comma separated string.
+		 *
+		 * \author Scott K Logan
+		 *
+		 * \param limit Speed limit value to convert
+		 *
+		 * \returns C++ string containing the limiting cause descriptions
+		 */
+		std::string LimitToStr( const uint16_t limit ) const;
+
+		/*!
+		 * \brief Mutex used for dynamic reconfigure
+		 */
+		boost::recursive_mutex dyn_re_mutex;
+
+		/*!
+		 * \brief Private NodeHanlde used to interface with ROS
+		 */
+		ros::NodeHandle nh_priv;
+		/*!
+		 * \brief Subscription to speed control data
+		 */
+		ros::Subscriber speed_sub;
+		/*!
+		 * \brief Safe start service provider
+		 */
+		ros::ServiceServer safe_start_srv;
+		/*!
+		 * \brief Emergency stop service provider
+		 */
+		ros::ServiceServer estop_srv;
+		/*!
+		 * \brief Dynamic reconfigure server
+		 */
+		dynamic_reconfigure::Server<pololu_smc_driver::SMCDriverConfig> dyn_re;
+		/*!
+		 * \brief Dynamic reconfigure callback handle
+		 */
+		dynamic_reconfigure::Server<pololu_smc_driver::SMCDriverConfig>::CallbackType dyn_re_cb_type;
+		/*!
+		 * \brief Timer for calling the diagnostic update function
+		 */
+		ros::WallTimer diag_timer;
+		/*!
+		 * \brief Diagnostic updater
+		 */
+		diagnostic_updater::Updater diag;
+		/*!
+		 * \brief Normal acceptable update rate minimum
+		 */
+		double min_update_rate;
+		/*!
+		 * \brief Normal acceptable update rate maximum
+		 */
+		double max_update_rate;
+		/*!
+		 * \brief Diagnostic rate for speed update
+		 */
+		diagnostic_updater::FrequencyStatus diag_up_freq;
+
+		/*!
+		 * \brief SMC device handle to use when interfacing with the standalone
+		 *   driver.
+		 */
+		int smcd;
+		/*!
+		 * \brief Serial number of the device with which we should be interfacing, or
+		 *   a blank string for the first available device.
+		 */
+		std::string serial;
+	};
+}
+
+#endif /* _smc_driver_hpp */
---
+++ src/smc_driver.cpp
@@ -0,0 +1,476 @@
+/***************************************************************************//**
+ * \file smc_driver.cpp
+ *
+ * \brief ROS Implementation of the C Driver
+ * \author Scott K Logan
+ * \date January 07, 2013
+ *
+ * Defined here is a class which wraps the basic C driver and sets up data
+ * pipelines with ROS. The features include basic speed control, dynamic
+ * reconfigure, diagnostics, and services for estop/safe-start.
+ *
+ * \section license License (BSD-3)
+ * Copyright (c) 2013, Scott K Logan\n
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of Willow Garage, Inc. nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+#include "pololu_smc_driver/smc_driver.hpp"
+
+#include <ros/ros.h>
+#include <dynamic_reconfigure/server.h>
+#include <driver_base/SensorLevels.h>
+
+namespace pololu_smc_driver
+{
+	SMCDriver::SMCDriver( const ros::NodeHandle &_nh_priv, const std::string _serial ) :
+		nh_priv( _nh_priv ),
+		dyn_re( dyn_re_mutex, nh_priv ),
+		dyn_re_cb_type( boost::bind( &SMCDriver::DynReCB, this, _1, _2) ),
+		min_update_rate( 10.0 ),
+		max_update_rate( 100.0 ),
+		diag_up_freq( diagnostic_updater::FrequencyStatusParam( &min_update_rate, &max_update_rate, 0.1, 5 ) ),
+		smcd( -1 ),
+		serial( _serial )
+	{
+		smc_init( );
+		diag.setHardwareID( "Pololu SMC (unknown serial)" );
+		diag.add( "Pololu SMC Status", this, &SMCDriver::DiagCB );
+		diag.add( diag_up_freq );
+		diag_timer = nh_priv.createWallTimer( ros::WallDuration( 1 ), &SMCDriver::TimerCB, this );
+	}
+
+	SMCDriver::~SMCDriver( )
+	{
+		SMCClose( );
+		smc_exit( );
+	}
+
+	bool SMCDriver::set_speed( float spd )
+	{
+		if( !SMCStat( ) )
+			return false;
+
+		short int dir = 1;
+		if( spd < 0.0 )
+		{
+			dir = -1;
+			spd *= -1;
+		}
+		spd *= 3200;
+		return smc_set_speed( smcd, (int)(spd + .5), dir, 2000 );
+	}
+
+	void SMCDriver::SpeedCB( const std_msgs::Float32Ptr &msg )
+	{
+		set_speed( msg->data );
+	}
+
+	void SMCDriver::DynReCB( pololu_smc_driver::SMCDriverConfig &cfg, const uint32_t lvl )
+	{
+		/// \todo Implement settings changes
+		if( !SMCStat( ) )
+			return;
+
+		struct SmcSettings set;
+
+		if( smc_get_settings( smcd, &set, 5000 ) < 0 )
+			return;
+
+		set.neverSuspend = cfg.neverSuspend;
+		set.uartResponseDelay = cfg.uartResponseDelay;
+		set.useFixedBaudRate = cfg.useFixedBaudRate;
+		set.disableSafeStart = cfg.disableSafeStart;
+		set.fixedBaudRateRegister = cfg.fixedBaudRateRegister;
+		set.speedUpdatePeriod = cfg.speedUpdatePeriod;
+		set.commandTimeout = cfg.commandTimeout;
+		set.serialDeviceNumber = cfg.serialDeviceNumber;
+		set.crcMode = cfg.crcMode;
+		set.overTempMin = cfg.overTempMin;
+		set.overTempMax = cfg.overTempMax;
+		set.inputMode = cfg.inputMode;
+		set.pwmMode = cfg.pwmMode;
+		set.pwmPeriodFactor = cfg.pwmPeriodFactor;
+		set.mixingMode = cfg.mixingMode;
+		set.minPulsePeriod = cfg.minPulsePeriod;
+		set.maxPulsePeriod = cfg.maxPulsePeriod;
+		set.rcTimeout = cfg.rcTimeout;
+		set.ignorePotDisconnect = cfg.ignorePotDisconnect;
+		set.tempLimitGradual = cfg.tempLimitGradual;
+		set.consecGoodPulses = cfg.consecGoodPulses;
+		set.motorInvert = cfg.motorInvert;
+		set.speedZeroBrakeAmount = cfg.speedZeroBrakeAmount;
+		set.ignoreErrLineHigh = cfg.ignoreErrLineHigh;
+		set.vinMultiplierOffset = cfg.vinMultiplierOffset;
+		set.lowVinShutoffTimeout = cfg.lowVinShutoffTimeout;
+		set.lowVinShutoffMv = cfg.lowVinShutoffMv;
+		set.serialMode = cfg.serialMode;
+
+		if( smc_set_settings( smcd, &set, 5000 ) < 0 )
+			return;
+	}
+
+	bool SMCDriver::SMCOpen( )
+	{
+		const char *ser = NULL;
+
+		if( !smc_stat( smcd ) )
+			return true;
+
+		if( serial.length( ) )
+			ser = serial.c_str( );
+
+		if( ( smcd = smc_open( ser ) ) < 0 )
+			return false;
+
+		char mySerial[256];
+		smc_get_serial( smcd, mySerial );
+		diag.setHardwareIDf( "Pololu SMC %s", mySerial );
+
+		if( !refresh_settings( ) )
+		{
+			smc_close( smcd );
+			smcd = -1;
+			return false;
+		}
+
+		dyn_re.setCallback( dyn_re_cb_type );
+		speed_sub = nh_priv.subscribe( "speed", 1, &SMCDriver::SpeedCB, this );
+		safe_start_srv = nh_priv.advertiseService( "safe_start", &SMCDriver::SafeStartCB, this );
+		estop_srv = nh_priv.advertiseService( "estop", &SMCDriver::EStopCB, this );
+
+		return true;
+	}
+
+	void SMCDriver::SMCClose( )
+	{
+		estop_srv.shutdown( );
+		safe_start_srv.shutdown( );
+		speed_sub.shutdown( );
+		dyn_re.clearCallback( );
+		smc_close( smcd );
+		smcd = -1;
+	}
+
+	bool SMCDriver::SMCStat( )
+	{
+		if( smc_stat( smcd ) )
+		{
+			if( !SMCOpen( ) )
+				return false;
+		}
+		return true;
+	}
+
+	void SMCDriver::TimerCB( const ros::WallTimerEvent &e )
+	{
+		diag.update( );
+
+		// This seems to do two things:
+		// - restart the timer (otherwise the diagnostic_updater limits us and we hit 1/2 of the time)
+		// - update the timer if the diagnostic period changed
+		diag_timer.setPeriod( ros::WallDuration( diag.getPeriod( ) ) );
+	}
+
+	void SMCDriver::DiagCB( diagnostic_updater::DiagnosticStatusWrapper &stat )
+	{
+		if( !SMCStat( ) )
+		{
+			stat.summary( diagnostic_msgs::DiagnosticStatus::ERROR, "Disconnected" );
+			return;
+		}
+
+		stat.summary( diagnostic_msgs::DiagnosticStatus::OK, "SMC status OK" );
+
+		int r;
+
+		// Firmware Version
+		unsigned short int maj;
+		unsigned short int min;
+		if( ( r = smc_get_fw_version( smcd, &maj, &min, 1000 ) ) < 0 )
+		{
+			stat.summary( diagnostic_msgs::DiagnosticStatus::WARN, "Failed to fetch fw_version" );
+			if( r == SMC_ERROR_NO_DEVICE )
+				SMCClose( );
+		}
+		else
+		{
+			float fw_ver = min;
+			while( fw_ver > 1.0 )
+				fw_ver /= 10;
+			fw_ver += maj;
+			stat.add( "fw_version", fw_ver );
+		}
+
+		// Other Variables
+		struct SmcVariables vars;
+		if( ( r = smc_get_variables( smcd, &vars, 5000 ) ) < 0 )
+		{
+			stat.summary( diagnostic_msgs::DiagnosticStatus::WARN, "Failed to fetch SMC variables" );
+			if( r == SMC_ERROR_NO_DEVICE )
+				SMCClose( );
+		}
+		else
+		{
+			if( vars.errorStatus )
+				stat.summary( diagnostic_msgs::DiagnosticStatus::ERROR, "SMC is reporting errors" );
+			else if( vars.errorOccurred )
+				stat.summary( diagnostic_msgs::DiagnosticStatus::WARN, "SMC has recorded errors" );
+			stat.add( "errorStatus", ErrorToStr( vars.errorStatus ) );
+			stat.add( "errorOccurred", ErrorToStr( vars.errorOccurred ) );
+			stat.add( "limitStatus", LimitToStr( vars.limitStatus ) );
+			stat.add( "targetSpeed", vars.targetSpeed / 32.0 );
+			stat.add( "speed", vars.speed / 32.0 );
+			stat.add( "brakeAmount", ( vars.brakeAmount == 255 ) ? 0.0 : vars.brakeAmount / .320 );
+			stat.add( "vin", vars.vinMv / 1000.0 );
+			stat.add( "temperature", vars.temperature / 10.0 );
+			stat.add( "rcPeriod", vars.rcPeriod / 1000.0 );
+			stat.add( "baudRate", vars.baudRateRegister );
+			stat.add( "time", vars.timeMs / 1000.0 );
+		}
+	}
+
+	std::string SMCDriver::ErrorToStr( const uint16_t error ) const
+	{
+		std::string str;
+		bool found = false;
+
+		if( error & SmcErrorSafeStart )
+		{
+			str += "Safe-Start";
+			found = true;
+		}
+		if( error & SmcErrorChannelInvalid )
+		{
+			if( found )
+				str += ", ";
+			str += "Invalid Channel";
+			found = true;
+		}
+		if( error & SmcErrorSerial )
+		{
+			if( found )
+				str += ", ";
+			str += "Serial Comm";
+			found = true;
+		}
+		if( error & SmcErrorCommandTimeout )
+		{
+			if( found )
+				str += ", ";
+			str += "Command Timeout";
+			found = true;
+		}
+		if( error & SmcErrorLimitSwitch )
+		{
+			if( found )
+				str += ", ";
+			str += "Kill/Limit Switch";
+			found = true;
+		}
+		if( error & SmcErrorVinLow )
+		{
+			if( found )
+				str += ", ";
+			str += "Low Voltage";
+			found = true;
+		}
+		if( error & SmcErrorVinHigh )
+		{
+			if( found )
+				str += ", ";
+			str += "High Voltage";
+			found = true;
+		}
+		if( error & SmcErrorTemperatureHigh )
+		{
+			if( found )
+				str += ", ";
+			str += "High Temperature";
+			found = true;
+		}
+		if( error & SmcErrorMotorDriverError )
+		{
+			if( found )
+				str += ", ";
+			str += "Motor Driver Fault";
+			found = true;
+		}
+		if( error & SmcErrorErrLineHigh )
+		{
+			if( found )
+				str += ", ";
+			str += "Error Line High";
+			found = true;
+		}
+
+		if( !str.length( ) )
+			str = "None";
+		return str;
+	}
+
+	std::string SMCDriver::LimitToStr( const uint16_t limit ) const
+	{
+		std::string str;
+		bool found = false;
+
+		if( limit & SmcLimitStatusStartedState )
+		{
+			str += "Safe-Start";
+			found = true;
+		}
+		if( limit & SmcLimitStatusTemperature )
+		{
+			if( found )
+				str += ", ";
+			str += "Temperature";
+			found = true;
+		}
+		if( limit & SmcLimitStatusMaxSpeed )
+		{
+			if( found )
+				str += ", ";
+			str += "Max Speed";
+			found = true;
+		}
+		if( limit & SmcLimitStatusStartingSpeed )
+		{
+			if( found )
+				str += ", ";
+			str += "Min Speed";
+			found = true;
+		}
+		if( limit & SmcLimitStatusAcceleration )
+		{
+			if( found )
+				str += ", ";
+			str += "Max Acceleration";
+			found = true;
+		}
+		if( limit & SmcLimitStatusRc1 )
+		{
+			if( found )
+				str += ", ";
+			str += "RC Ch 1 Limit";
+			found = true;
+		}
+		if( limit & SmcLimitStatusRc2 )
+		{
+			if( found )
+				str += ", ";
+			str += "RC Ch 2 Limit";
+			found = true;
+		}
+		if( limit & SmcLimitStatusAnalog1 )
+		{
+			if( found )
+				str += ", ";
+			str += "Analog Ch 1 Limit";
+			found = true;
+		}
+		if( limit & SmcLimitStatusAnalog2 )
+		{
+			if( found )
+				str += ", ";
+			str += "Analog Ch 2 Limit";
+			found = true;
+		}
+		if( limit & SmcLimitStatusUsbKill )
+		{
+			if( found )
+				str += ", ";
+			str += "USB Killswitch";
+			found = true;
+		}
+
+		if( !str.length( ) )
+			str = "None";
+		return str;
+	}
+
+	bool SMCDriver::SafeStartCB( std_srvs::Empty::Request &req, std_srvs::Empty::Response &res )
+	{
+		if( !SMCStat( ) )
+			return false;
+
+		return ( smc_resume( smcd, 2000 ) >= 0 );
+	}
+
+	bool SMCDriver::EStopCB( std_srvs::Empty::Request &req, std_srvs::Empty::Response &res )
+	{
+		if( !SMCStat( ) )
+			return false;
+
+		return ( smc_stop( smcd, 2000 ) >= 0 );
+	}
+
+	bool SMCDriver::refresh_settings( )
+	{
+		if( !SMCStat( ) )
+			return false;
+
+		struct SmcSettings set;
+
+		if( smc_get_settings( smcd, &set, 5000 ) < 0 )
+			return false;
+
+		struct pololu_smc_driver::SMCDriverConfig cfg;
+
+		cfg.neverSuspend = set.neverSuspend;
+		cfg.uartResponseDelay = set.uartResponseDelay;
+		cfg.useFixedBaudRate = set.useFixedBaudRate;
+		cfg.disableSafeStart = set.disableSafeStart;
+		cfg.fixedBaudRateRegister = set.fixedBaudRateRegister;
+		cfg.speedUpdatePeriod = set.speedUpdatePeriod;
+		cfg.commandTimeout = set.commandTimeout;
+		cfg.serialDeviceNumber = set.serialDeviceNumber;
+		cfg.crcMode = set.crcMode;
+		cfg.overTempMin = set.overTempMin;
+		cfg.overTempMax = set.overTempMax;
+		cfg.inputMode = set.inputMode;
+		cfg.pwmMode = set.pwmMode;
+		cfg.pwmPeriodFactor = set.pwmPeriodFactor;
+		cfg.mixingMode = set.mixingMode;
+		cfg.minPulsePeriod = set.minPulsePeriod;
+		cfg.maxPulsePeriod = set.maxPulsePeriod;
+		cfg.rcTimeout = set.rcTimeout;
+		cfg.ignorePotDisconnect = set.ignorePotDisconnect;
+		cfg.tempLimitGradual = set.tempLimitGradual;
+		cfg.consecGoodPulses = set.consecGoodPulses;
+		cfg.motorInvert = set.motorInvert;
+		cfg.speedZeroBrakeAmount = set.speedZeroBrakeAmount;
+		cfg.ignoreErrLineHigh = set.ignoreErrLineHigh;
+		cfg.vinMultiplierOffset = set.vinMultiplierOffset;
+		cfg.lowVinShutoffTimeout = set.lowVinShutoffTimeout;
+		cfg.lowVinShutoffMv = set.lowVinShutoffMv;
+		cfg.serialMode = set.serialMode;
+
+		boost::recursive_mutex::scoped_lock lock( dyn_re_mutex );
+		dyn_re.updateConfig( cfg );
+
+		return true;
+	}
+}
+
